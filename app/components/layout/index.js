ce7bf8d4bcef4c5ab24623c6e2553a3f634ff65d
Brandon Baris <brandonbaris@Brandons-MacBook-Pro.local>
2017-10-31 16:42:10 -1000

    changes emmet style links and uses attribute tags for href

--- app/components/layout/index.js
+++ app/components/layout/index.js
@@ -2,31 +2,31 @@ import m from 'mithril'
 
 export default {
 	view: function(vnode){
-		return m("main.layout", [
-			m("nav.menu",
-				m("ul.menu.item", [
-						m("li.menu.item",
-							m("a[href='/home']", { oncreate: m.route.link }, "Home" )
+		return m('main.layout', [
+			m('nav.menu',
+				m('ul.menu.item', [
+						m('li.menu.item',
+							m('a', { href: '/home', oncreate: m.route.link }, 'Home' )
 						),
-						m("li.menu.item",
-							m("a[href='/portfolio']", { oncreate: m.route.link }, "Portfolio" )
+						m('li.menu.item',
+							m('a', { href:'/portfolio', oncreate: m.route.link }, 'Portfolio' )
 						),
-						m("li.menu.item",
-							m("a[href='/cv']", { oncreate: m.route.link }, "CV" )
+						m('li.menu.item',
+							m('a', { href:'/cv', oncreate: m.route.link }, 'CV' )
 						),
-						m("li.menu.item",
-							m("a[href='https://github.com/brandonbaris']", "GitHub" )
+						m('li.menu.item',
+							m('a', { href: 'https://github.com/brandonbaris'}, 'GitHub' )
 						),
-						m("li.menu.item",
-							m("a[href='https://www.linkedin.com/in/brandonbaris/']", "LinkedIn" )
+						m('li.menu.item',
+							m('a', { href: 'https://www.linkedin.com/in/brandonbaris/' }, 'LinkedIn' )
 						)
 					]
 				)
 			),
-			m("header","HEADER DESU"),
-			m("section", vnode.children),
-			m("footer",
-				m("a[href='mailto:brandonbaris@gmail.com?subject=HI BRANDON']", new Date().getFullYear())
+			m('header','HEADER DESU'),
+			m('section', vnode.children),
+			m('footer',
+				m('a', { href: 'mailto:brandonbaris@gmail.com?subject=HI BRANDON'}, new Date().getFullYear())
 			)
 		])
 	}
