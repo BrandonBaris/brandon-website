ba0e569b58d1a88315f5a163735e8f75dd54934c
Brandon Baris <brandonbaris@Brandons-MacBook-Pro.local>
2017-10-31 16:42:46 -1000

    Rough linking of portfolio model data to view.

--- app/components/portfolio/index.js
+++ app/components/portfolio/index.js
@@ -6,7 +6,13 @@ var List = {
 	view: function(){
 		return m('ul.portfolio-list',[
 			Data.list.map(function(data){
-				return m('li.portfolio-list-item', m('a.data-list-item', {href: '/portfolio/' + data.id, oncreate: m.route.link}, data.project_name + ' ' + data.description))
+				return m('li.portfolio-list-item', [
+						m('a.portfolio-list-item-link', {href: '/portfolio/' + data.id, oncreate: m.route.link}, data.project_name),
+						m('br'),
+						m('.portfolio-list-item-end_date', data.end_date),
+						m('.portfolio-list-item-description', data.description)
+					]
+				)
 			})
 		])
 	}
@@ -15,7 +21,25 @@ var List = {
 var Show = {
 	oninit: function(vnode){ Data.load(vnode.attrs.id) },
 	view: function(){
-		return m('.project_name', Data.current.project_name + ' ' + Data.current.description)
+		return m('.portfolio',[
+			m('a.back_button', { href: '/portfolio', oncreate: m.route.link }, '<< BACK'),
+			m('.project_name', Data.current.project_name),
+			m('.start_date', Data.current.start_date),
+			m('.end_date', Data.current.end_date),
+			m('.portfolio-tasks',[
+				m('ul.portfolio-tasks-list',
+					Data.current.tasks.map( function(task){
+						return m('li.portfolio-tasks-list-item', task)
+					})
+				)
+			]),
+			m('.tasks', Data.current.tasks),
+			m('.uri', Data.current.uri),
+			m('img.portfolio-item-thumbnail', { src: Data.current.thumbnail.value, alt: Data.current.thumbnail.description }),
+			Data.current.imgs.map( function(img){
+				return m('img', { src: img.value, alt: img.description })
+			})
+		])
 	}
 }
 
--- app/components/portfolio/portfolio.model.js
+++ app/components/portfolio/portfolio.model.js
@@ -6,32 +6,13 @@ var Data = {
 	loadJSON: function(){
 		return Data.list = portfolio_data.data
 	},
-	// loadList: function() {
-	// 	return m.request({
-	// 		method: "GET",
-	// 		url: "https://rem-rest-api.herokuapp.com/api/users",
-	// 		withCredentials: true
-	// 	})
-	// 	.then( function(result){
-	// 		console.log(result.data)
-	// 		Data.list = result.data
-	// 	})
 	current: {},
 	load: function(id){
 		var _data = portfolio_data.data.find(function(data){
-			console.log('asd',id)
 			return data.id == id
 		})
-		console.log("data",_data);
-		return Data.current = ( _data !== undefined ) ? _data : Data.current = { firstName: "HIHOI", lastName: "AIOFOA", id: 0 }
-		// return m.request({
-		// 	method: "GET",
-		// 	url: "https://rem-rest-api.herokuapp.com/api/users/" + id,
-		// 	withCredentials: true
-		// })
-		// .then( function(result){
-		// 	Data.current = result
-		// })
+		console.log('data',_data);
+		return Data.current = ( _data !== undefined ) ? _data : Data.current = { firstName: 'HIHOI', lastName: 'AIOFOA', id: 0 }
 	}
 }
 
