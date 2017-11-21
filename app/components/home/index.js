8c30f05dabb6c4a9b3929d1ab58a11dcd016aa1f
Brandon Baris <brandonbaris@Brandons-MacBook-Pro.local>
2017-11-21 12:38:59 -1000

    adds some styles to home view

--- app/components/home/index.js
+++ app/components/home/index.js
@@ -1,7 +1,13 @@
 import m from 'mithril'
+import './home.css'
 
 export default {
 	view: function(vnode){
-		return m(".home", "Brandon's Website")
+		return m('.home-content',[
+			m(".home", [
+				m('.home-header','Welcome'),
+				m('.home-body','Work In Progress...')
+			])
+		])
 	}
 }
