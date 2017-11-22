4d4a38481cef7fad147b1ad3ec56f56e6acd4e1b
Brandon Baris <brandonbaris@Brandons-MacBook-Pro.local>
2017-11-22 06:31:21 -1000

    adds sample images for all portfolio items

--- app/assets/images/index.js
+++ app/assets/images/index.js
@@ -2,6 +2,15 @@ import p_air_01 from './p_airship_01.png'
 import p_air_02 from './p_airship_02.png'
 import p_gaptcha_01 from './p_gaptcha_01.png'
 import p_gaptcha_02 from './p_gaptcha_02.png'
+import p_mantle_01 from './p_mantle_01.png'
+import p_disrupt_01 from './p_disrupt_01.png'
+import p_waters_01 from './p_waters_01.png'
+import p_micro_01 from './p_micro_01.png'
+import p_vivid_01 from './p_vivid_01.png'
+import p_hello_01 from './p_hello_01.png'
+import p_twc_01 from './p_twc_01.png'
+import p_uhero_01 from './p_uhero_01.png'
+import p_uhero_02 from './p_uhero_02.png'
 import me from './me.png'
 
 export default {
@@ -9,5 +18,14 @@ export default {
 	p_air_01,
 	p_air_02,
 	p_gaptcha_01,
-	p_gaptcha_02
+	p_gaptcha_02,
+	p_mantle_01,
+	p_disrupt_01,
+	p_waters_01,
+	p_micro_01,
+	p_vivid_01,
+	p_hello_01,
+	p_twc_01,
+	p_uhero_01,
+	p_uhero_02,
 }
